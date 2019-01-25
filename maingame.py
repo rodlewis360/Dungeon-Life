384d4bc5c25f1457ab076c821bb2de9f41187557
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-01-24 16:36:04 -0800

    added intro

--- maingame.py
+++ maingame.py
@@ -1,1 +1,13 @@
 import random
+from time import sleep()
+def DungeonLife():
+    print("You wake up in a dungeon, feeling natious.")
+    sleep(2.5)
+    print("You wonder why and how you got here, but you cannot worry now, for some reason.")
+    sleep(2.5)
+    print("You notice that you have a stick strapped to your side.")
+    sleep(2.5)
+    print("You also realize that you can manipulate and create fire.")
+    sleep(2.5)
+    while True:
+        
